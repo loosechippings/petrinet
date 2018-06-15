53a9e4a76f607863e9584cbe646d0f99c9b1776c
n1ckay3r5@gmail.com <n1ckay3r5@gmail.com>
2018-06-15 14:40:10 +0100

    tidy test

--- src/test/java/loosechippings/petrinet/PetrinetTest.java
+++ src/test/java/loosechippings/petrinet/PetrinetTest.java
@@ -20,14 +20,18 @@ public class PetrinetTest {
    Place<StatusUpdate> receivedStatus;
    Place<Instruction> instructionClosed;
 
+   TokenDescriptor<Trade> tradeTokenDescriptor = new TokenDescriptor<>(Trade.class, "trade");
+   TokenDescriptor<Instruction> instructionTokenDescriptor = new TokenDescriptor<>(Instruction.class, "instruction");
+   TokenDescriptor<StatusUpdate> statusUpdateTokenDescriptor = new TokenDescriptor<>(StatusUpdate.class, "statusUpdate");
+
    @Before
    public void init() {
-      receivedTrade = new Place<>("Received Trade", new TokenDescriptor<>(Trade.class, "trade"));
-      settlementInstructed = new Place<>("Settlement Instructed", new TokenDescriptor<>(Instruction.class, "instruction"));
-      instructionOpen = new Place<>("Instruction Open", new TokenDescriptor<>(Instruction.class, "instruction"));
-      receivedStatus = new Place<>("Received Status", new TokenDescriptor<>(StatusUpdate.class, "statusUpdate"));
-      instructionClosed = new Place<>("Instruction Closed", new TokenDescriptor<>(Instruction.class, "instruction"));
-      Place<Instruction> instructionWithStatusApplied = new Place<>("Instruction with status", new TokenDescriptor<>(Instruction.class, "instruction"));
+      receivedTrade = new Place<>("Received Trade", tradeTokenDescriptor);
+      settlementInstructed = new Place<>("Settlement Instructed", instructionTokenDescriptor);
+      instructionOpen = new Place<>("Instruction Open", instructionTokenDescriptor);
+      receivedStatus = new Place<>("Received Status", statusUpdateTokenDescriptor);
+      instructionClosed = new Place<>("Instruction Closed", instructionTokenDescriptor);
+      Place<Instruction> instructionWithStatusApplied = new Place<>("Instruction with status", instructionTokenDescriptor);
 
       Transition<Instruction> instructSettlement = new Transition<>("Instruct settlement", this::instructTrade);
       Transition<Instruction> t1 = new Transition<>("t1", this::foo);
@@ -64,7 +68,7 @@ public class PetrinetTest {
 
    public Instruction instructTrade(Context context) {
       System.out.println("creating Instruction from Trade");
-      Trade t = Trade.class.cast(context.getToken(new TokenDescriptor<>(Trade.class, "trade")));
+      Trade t = Trade.class.cast(context.getToken(tradeTokenDescriptor));
       Instruction i = new Instruction();
       i.setTradeReference(t.getReference());
       i.setStatus(Instruction.Status.SENT);
@@ -73,13 +77,13 @@ public class PetrinetTest {
 
    public Instruction foo(Context context) {
       System.out.println("triggered foo");
-      return context.getToken(new TokenDescriptor<>(Instruction.class, "instruction"));
+      return context.getToken(instructionTokenDescriptor);
    }
 
    public Instruction applyStatus(Context context) {
       System.out.println("triggered applyStatus");
-      Instruction i = context.getToken(new TokenDescriptor<>(Instruction.class, "instruction"));
-      StatusUpdate s = context.getToken(new TokenDescriptor<>(StatusUpdate.class, "statusUpdate"));
+      Instruction i = context.getToken(instructionTokenDescriptor);
+      StatusUpdate s = context.getToken(statusUpdateTokenDescriptor);
       switch (s.getStatus()) {
          case MATCHED:i.setStatus(Instruction.Status.MATCHED);
             break;
@@ -92,12 +96,12 @@ public class PetrinetTest {
 
    public Instruction checkIfStatusOpen(Context context) {
       System.out.println("triggered checkIfStatusOpen");
-      return context.getToken(new TokenDescriptor<>(Instruction.class, "instruction"));
+      return context.getToken(instructionTokenDescriptor);
    }
 
    public Instruction checkIfStatusClosed(Context context) {
       System.out.println("triggered checkIfStatusClosed");
-      return context.getToken(new TokenDescriptor<>(Instruction.class, "instruction"));
+      return context.getToken(instructionTokenDescriptor);
    }
 
    @Test
